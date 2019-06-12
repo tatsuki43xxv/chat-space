dba1cd8a55c4ba5507e765f9a3648bcd2d045345
tatsuki sekiya <tatsuki43-z1x@ezweb.ne.jp>
2019-06-12 19:11:29 +0900

    メッセージ機能の追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,19 @@
 class UsersController < ApplicationController
-  def index
-  end
 
   def edit
   end
 
-  def updete
+  def update
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
   end
 end
