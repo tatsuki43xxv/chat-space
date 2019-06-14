34f9d0798fd4952531257d070a81ace4d1187ba6
tatsuki sekiya <tatsuki43-z1x@ezweb.ne.jp>
2019-06-14 16:02:06 +0900

    自動更新実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -8,7 +8,7 @@ $(document).on('turbolinks:load', function(){
                       ${message.user_name}
                     </p>
                     <p class="info__date">
-                      ${message.date}
+                      ${message.create_at}
                     </p>
                   </div>
                   <div class="body">
@@ -16,28 +16,32 @@ $(document).on('turbolinks:load', function(){
                     ${body}
                     </div>
                     ${img}
+                    </div>
                   </div>
                 </div>`
     return html;
   }
+  function scroll_view() {
+    var position = $('.messages').get(0).scrollHeight;
+      $('.messages').animate({scrollTop: position});
+  }
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var message = new FormData(this);
-    var url = $(this).attr('action')
+    var url = $(this).attr('action');
     $.ajax({
       url: url,
       type: "POST",
       data: message,
       dataType: 'json',
       processData: false,
-      contentType: false
+      contentType: false,
     })
     .done(function(data){
       var html = buildHTML(data);
       $('.messages').append(html);
-      $('#new_message')[0].reset();
-      var position = $('.messages').get(0).scrollHeight;
-      $('.messages').animate({scrollTop: position});
+      $('.new_message').get(0).reset();
+      scroll_view();
     })
     .fail(function(data){
       alert('エラーが発生したためメッセージは送信できませんでした。');
@@ -46,4 +50,32 @@ $(document).on('turbolinks:load', function(){
       $('.form__submit').prop('disabled', false);
     })
   })
+  var reloadMessages = function() {
+    var last_message_id = $('.messages').children().last().data('id');
+    var reload_url = window.location.href
+    reload_url_pattern = '/messages';
+    api_url = reload_url.replace(reload_url_pattern, '/api/messages');
+    if (reload_url.match(/^(?=.*groups)(?=.*messages)/)) {
+      $.ajax({
+        url: api_url,
+        type: 'get',
+        dataType: 'json',
+        data: {id: last_message_id},
+      })
+      .done(function(messages) {
+        var insertHTML = '';
+        if (messages.length !== 0){
+          messages.forEach(function(message) {
+            insertHTML += buildHTML(message);
+            scroll_view();
+          });
+        }
+        $('.messages').append(insertHTML);
+      })
+      .fail(function() {
+        alert('自動更新に失敗しました');
+      });
+    }
+  };
+  setInterval(reloadMessages, 5000);
 });
