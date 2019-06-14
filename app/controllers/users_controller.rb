14e756665659b657dfa6525304f66b3305626968
tatsuki sekiya <tatsuki43-z1x@ezweb.ne.jp>
2019-06-14 14:42:23 +0900

    インクリメンタルサーチ実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,13 @@ class UsersController < ApplicationController
     end
   end
 
+  def index
+    @users = User.where('name LIKE(?)', "#{params[:keyword]}%").where.not(id: current_user.id)
+    respond_to do |format|
+      format.json
+    end
+  end
+
   private
 
   def user_params
