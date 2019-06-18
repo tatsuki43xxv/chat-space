d08f51648cb8711d4cea692a0276c609ed43cff5
tatsuki sekiya <tatsuki43-z1x@ezweb.ne.jp>
2019-06-18 15:03:24 +0900

    自動デプロイ修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,16 +1,16 @@
 # config valid only for current version of Capistrano
-lock '<Capistranoのバージョン>'
+lock '<3.11.0>'
 
-set :application, '自身のアプリケーション名'
-set :repo_url,  'git@github.com:<Githubのユーザー名>/<レポジトリ名>.git'
+set :application, 'chat-space'
+set :repo_url,  'git@github.com:<tatsuki43xxv>/<chat-space>.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
-set :rbenv_ruby, '<このアプリで使用しているrubyのバージョン>' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
+set :rbenv_ruby, '<2.5.1>' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['<ローカルPCのEC2インスタンスのSSH鍵(pem)へのパス>']  ※例：~/.ssh/key_pem.pem
+                  keys: ['<~/.ssh/tatsuki_sekiya.pem>']
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
