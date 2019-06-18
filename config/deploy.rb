289c4222ce4e0f92b9b89244e1d40034c288ce54
tatsuki sekiya <tatsuki43-z1x@ezweb.ne.jp>
2019-06-18 15:40:25 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,15 +2,15 @@
 lock '~> 3.11.0'
 
 set :application, 'chat-space'
-set :repo_url,  'git@github.com:<tatsuki43xxv>/<chat-space>.git'
+set :repo_url,  'git@github.com:tatsuki43xxv/chat-space.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
-set :rbenv_ruby, '<2.5.1>' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
+set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['<~/.ssh/tatsuki_sekiya.pem>']
+                  keys: ['~/.ssh/tatsuki_sekiya.pem']
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
